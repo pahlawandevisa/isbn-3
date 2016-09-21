fd11c56168f929ba9238cb81e65515eaa196a8c5
gg_baonq <baonq@greenglobal.vn>
2016-09-21 16:01:32 +0700

    Fix bug styles follow eslint

--- src/index.js
+++ src/index.js
@@ -1,50 +1,50 @@
-var isValidIsbn = function(strIsbn, callback) {
+var isValidIsbn = function isValidIsbn(strIsbn, callback) {
 
-    var sum,
-        weight,
-        digit,
-        check,
-        i,
-        result;
+  var check,
+    digit,
+    i,
+    result,
+    sum,
+    weight;
 
-    var str = strIsbn.replace(/[^0-9X]/gi, '');
+  var str = strIsbn.replace(/[^0-9X]/gi, '');
 
-    if (str.length != 10 && str.length != 13) {
-        result = false;
+  if (str.length !== 10 && str.length !== 13) {
+    result = false;
+  }
+
+  if (str.length === 13) {
+    sum = 0;
+    for (i = 0; i < 12; i++) {
+      digit = parseInt(str[i], 10);
+      if (i % 2 === 1) {
+        sum += 3 * digit;
+      } else {
+        sum += digit;
+      }
     }
+    check = (10 - sum % 10) % 10;
+    result = check === parseInt(str[str.length - 1], 10);
+  }
 
-    if (str.length == 13) {
-        sum = 0;
-        for (i = 0; i < 12; i++) {
-            digit = parseInt(str[i]);
-            if (i % 2 == 1) {
-                sum += 3*digit;
-            } else {
-                sum += digit;
-            }
-        }
-        check = (10 - (sum % 10)) % 10;
-        result = (check == str[str.length-1]);
+  if (str.length === 10) {
+    weight = 10;
+    sum = 0;
+    for (i = 0; i < 9; i++) {
+      digit = parseInt(str[i], 10);
+      sum += weight * digit;
+      weight--;
     }
-
-    if (str.length == 10) {
-        weight = 10;
-        sum = 0;
-        for (i = 0; i < 9; i++) {
-            digit = parseInt(str[i]);
-            sum += weight*digit;
-            weight--;
-        }
-        check = 11 - (sum % 11);
-        if (check == 10) {
-            check = 'X';
-        }
-        result = (check == str[str.length-1].toUpperCase());
+    check = 11 - sum % 11;
+    if (check === 10) {
+      check = 'X';
     }
+    result = check === str[str.length - 1].toUpperCase();
+  }
 
-    callback(null, result);
+  callback(null, result);
 };
 
 module.exports = {
-    isValidIsbn: isValidIsbn
+  isValidIsbn
 };
